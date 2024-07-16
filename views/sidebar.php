cb287cdd901f684909125c6517077120f5bb324d
dipta-sdd <dipta5160@gmail.com>
2024-07-16 22:43:27 +0600

    sidebar skill

--- views/sidebar.php
+++ views/sidebar.php
@@ -133,10 +133,11 @@
             </li>
             <li class=" d-none u-Student">
               <a href="/course_list"
-                ><span class="fa-solid fa-wand-magic-sparkles"></span> &nbsp Course List</a
-              <a href="/my_skills"
-                ><span class="fa-solid fa-wand-magic-sparkles"></span> &nbsp Skills</a
-              >
+                ><span class="fa-solid fa-wand-magic-sparkles"></span> &nbsp Course List</a>
+            </li>
+            <li class=" d-none u-Student">
+              <a href="/course_list"
+                ><span class="fa-solid fa-wand-magic-sparkles"></span> &nbsp Course List</a>
             </li>
             <li class=" d-none u-Student">
               <a href="/program"
